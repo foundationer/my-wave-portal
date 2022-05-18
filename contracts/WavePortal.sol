4cf55fd7a06608d59b151c6dff2ea544c7df39c3
Lucas <foundationer@tuta.io>
2022-05-18 00:01:19 -0300

    Added deploy script.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,18 +6,43 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    address[] hallOfFame;
 
     constructor() {
+        hallOfFame = new address[](0);
         console.log("Hey you, I am a contract and I am smart");
     }
 
     function wave() public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+        updateHallOfFame(msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
+        showHallOfFame();
         return totalWaves;
+    }
+
+    function updateHallOfFame(address waver) private {
+      uint256 i = 0;
+      while(i < hallOfFame.length && hallOfFame[i] != waver) {
+        i++;
+      }
+      if(i == hallOfFame.length) {
+        hallOfFame.push(waver);
+      }
+    }
+
+    function showHallOfFame() private view {
+      uint256 i = 0;
+      console.log("HALL OF FAME:");
+      for(i=0; i < hallOfFame.length; i++) {
+        uint256 j = i + 1;
+        if(hallOfFame[i] != 0x0000000000000000000000000000000000000000) {
+          console.log("%d. %s", j, hallOfFame[i]);
+        }
+      }
     }
 }
