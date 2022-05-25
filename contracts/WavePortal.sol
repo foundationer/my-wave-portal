3f5c4b2009b7831bd77fd6136b5e62177e54d36b
Lucas <foundationer@tuta.io>
2022-05-24 21:12:48 -0300

    New contract to store waves.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,43 +6,59 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    address[] hallOfFame;
+
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
 
     constructor() {
-        hallOfFame = new address[](0);
         console.log("Hey you, I am a contract and I am smart");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
-        updateHallOfFame(msg.sender);
+        /*
+         * This is where I actually store the wave data in the array.
+         */
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+      return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
-        showHallOfFame();
-        return totalWaves;
-    }
-
-    function updateHallOfFame(address waver) private {
-      uint256 i = 0;
-      while(i < hallOfFame.length && hallOfFame[i] != waver) {
-        i++;
-      }
-      if(i == hallOfFame.length) {
-        hallOfFame.push(waver);
-      }
-    }
-
-    function showHallOfFame() private view {
-      uint256 i = 0;
-      console.log("HALL OF FAME:");
-      for(i=0; i < hallOfFame.length; i++) {
-        uint256 j = i + 1;
-        if(hallOfFame[i] != 0x0000000000000000000000000000000000000000) {
-          console.log("%d. %s", j, hallOfFame[i]);
-        }
-      }
+      // Optional: Add this line if you want to see the contract print the value!
+      // We'll also print it over in run.js as well.
+      console.log("We have %d total waves!", totalWaves);
+      return totalWaves;
     }
 }
